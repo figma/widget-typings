a1a10b26b00038e5708e81c22784ceb4b114e34c
Michael Yong <610102+ymichael@users.noreply.github.com>
2022-04-04 13:07:38 -0400

    Fix typings for our JSX IntrinsicAttributes (#11)
    Without this, developers have to manually declare `key` in their prop types of custom components when they use the key prop.
    
    See also: https://www.typescriptlang.org/docs/handbook/jsx.html#attribute-type-checking

--- index.d.ts
+++ index.d.ts
@@ -7,6 +7,18 @@ declare global {
   }
 
   ///////////////////////////////////////////////////////////////////////////////
+  // JSX
+  namespace JSX {
+    /**
+     * You shouldn't need to use this type since you never see these attributes
+     * inside your component or have to validate them.
+     */
+    interface IntrinsicAttributes {
+        key?: string | number | null | undefined;
+    }
+  }
+
+  ///////////////////////////////////////////////////////////////////////////////
   // Widget
 
   interface WidgetAPI {
